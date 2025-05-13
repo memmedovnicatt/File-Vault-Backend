70adcd5d7e7ccbd5b2da16855da890d09bfa79c8
memmedovnicatt <nicat.mammadov2022@khazar.org>
2025-05-13 23:38:59 +0400

    project-commit

--- src/main/java/com/nicat/filevaultbackend/configuration/MinIoConfig.java
+++ src/main/java/com/nicat/filevaultbackend/configuration/MinIoConfig.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.w3c.dom.Document;
 
 @Configuration
 @Getter
